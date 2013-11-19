1ad272611a9856f33d685b8ec3bcec1383b70500
s <s>
2013-11-20 05:36:11 +0800

    添加应用模板，来自cuda sample 的template模板工程

---
+++ app/template_gold.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright 1993-2009 NVIDIA Corporation.  All rights reserved.
+ *
+ * NVIDIA Corporation and its licensors retain all intellectual property and 
+ * proprietary rights in and to this software and related documentation and 
+ * any modifications thereto.  Any use, reproduction, disclosure, or distribution 
+ * of this software and related documentation without an express license 
+ * agreement from NVIDIA Corporation is strictly prohibited.
+ * 
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+// export C interface
+extern "C" 
+void computeGold( float* reference, float* idata, const unsigned int len);
+
+////////////////////////////////////////////////////////////////////////////////
+//! Compute reference data set
+//! Each element is multiplied with the number of threads / array length
+//! @param reference  reference data, computed but preallocated
+//! @param idata      input data as provided to device
+//! @param len        number of elements in reference / idata
+////////////////////////////////////////////////////////////////////////////////
+void
+computeGold( float* reference, float* idata, const unsigned int len) 
+{
+    const float f_len = static_cast<float>( len);
+    for( unsigned int i = 0; i < len; ++i) 
+    {
+        reference[i] = idata[i] * f_len;
+    }
+}
+
